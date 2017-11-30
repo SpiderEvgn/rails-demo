d76dfa6eb9b4efa81bc65061acd7cd21f86cbda8
Aaron <aaron.zhang@maximum.com>
2017-11-30 14:52:19 +0800

    add ali oss local env config

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,7 +3,7 @@
 
 CarrierWave.configure do |config|
 
-  config.storage = :aliyun
+  config.storage = ENV["IS_OSS_CONFIG"]=='true' ? :aliyun : :file
   config.aliyun_access_id  = ENV["ALIYUN_OSS_ACCESS_ID"]
   config.aliyun_access_key = ENV["ALIYUN_OSS_ACCESS_KEY"]
   config.aliyun_bucket     = ENV["ALIYUN_OSS_BUCKET"]
@@ -13,5 +13,3 @@ CarrierWave.configure do |config|
   # config.aliyun_host       = Setting.upload_url
 
 end
-
-
